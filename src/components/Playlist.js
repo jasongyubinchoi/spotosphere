ce2ffeec03286f2693025dbb2087dca069b3e2b5
norujumping <norujumping@kano.moe>
2021-10-23 16:34:25 -0400

    Add playlist iframe

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
 const Playlist = () => {
+    let weather = 'bad'
     return (
         <div className="widget-container">
-            place for playlist
+            {weather === 'good' ? (
+                <iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1DX4WYpdgoIcn6" width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+            ) : (
+                <iframe src="https://open.spotify.com/embed/playlist/0lU86qLkSQVI991j4BUTDF" width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+            )
+            }
         </div>
     )
 }
