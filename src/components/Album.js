0458cb70321fc944db144fd93e0fb1546356c463
cliff883 <clifford.yao@gmail.com>
2021-10-23 18:23:40 -0400

    Playlists

--- src/components/Album.js
+++ src/components/Album.js
@@ -3,8 +3,6 @@ import React from 'react'
 const Album = () => {
     return (
         <div className="widget-container">
-            <img border="0" src="cid:30A1D9A8E7C0405A89F2B3AB12878237@dns">
-            </img>
         </div>
     )
 }
