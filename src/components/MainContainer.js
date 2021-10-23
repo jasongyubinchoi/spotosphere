daed9927ce9fadcb03f4cbd8705456486cf2fb0c
norujumping <norujumping@kano.moe>
2021-10-23 18:25:31 -0400

    weather + time

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import React from 'react'
 import { Animated } from "react-animated-css"
 import Weather from './Weather';
@@ -5,17 +6,35 @@ import Album from './Album';
 import Playlist from './Playlist';
 
 const MainContainer = () => {
+    const [init, initSet] = useState(true)
+    const [weather, weatherSet] = useState('')
+    const [time, timeSet] = useState(new Date())
+    const updateWeather = () => {
+        let url = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=minutely,hourly,daily&appid=e7d52186e0e22ca020dd281f84eaed33"
+        fetch(url).then(res => res.json()).then((result) => {
+            let curWeather = result.current.weather[0].main
+            weatherSet(curWeather)
+        })
+    }
+    const updateDate = () => timeSet(new Date());
+
+    useEffect(() => {
+        updateWeather()
+        setInterval(updateWeather(), 5000);
+        setInterval(updateDate, 1000);
+    }, [])  //! Important! [] dependency means called only once
+
     return (
         <div className="main-container">
-            {/* <Animated animationInDelay={100} animationIn="zoomIn" animationOut="fadeOut" isVisible={true}> */}
-            <Weather />
-            {/* </Animated> */}
-            {/* <Animated animationInDelay={100} animationIn="zoomIn" animationOut="fadeOut" isVisible={true}> */}
-            <Album />
-            {/* </Animated> */}
-            {/* <Animated animationInDelay={100} animationIn="zoomIn" animationOut="fadeOut" isVisible={true}> */}
-            <Playlist />
-            {/* </Animated> */}
+            <Animated animationInDelay={100} animationIn="zoomIn" animationOut="fadeOut" isVisible={true}>
+                <Weather weather={weather} time={time} />
+            </Animated>
+            <Animated animationInDelay={100} animationIn="zoomIn" animationOut="fadeOut" isVisible={true}>
+                <Album />
+            </Animated>
+            <Animated animationInDelay={100} animationIn="zoomIn" animationOut="fadeOut" isVisible={true}>
+                <Playlist />
+            </Animated>
         </div>
     )
 }
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,9 +1,30 @@
 import React from 'react'
+// import cloudy from '../../img/cloudy.png'
+// import rain from '../../img/cloudy.png'
+// import sunny from '../../img/cloudy.png'
+// import thunder from '../../img/cloudy.png'
 
-const Weather = () => {
+const Weather = ({ weather, date }) => {
+    const renderWeather = (weather) => {
+        console.log(weather)
+        switch (weather) {
+            case 'sunny':
+                return <img src={process.env.PUBLIC_URL + '/img/sunny.png'} alt={weather} />
+            case 'Clouds':
+                return <img src={process.env.PUBLIC_URL + '/img/cloudy.png'} alt={weather} />
+            case 'rain':
+                return <img src={process.env.PUBLIC_URL + '/img/rain.gif'} alt={weather} />
+            case 'thunder':
+                return <img src={process.env.PUBLIC_URL + '/img/thunder.png'} alt={weather} />
+            default:
+                console.log(weather)
+        }
+    }
     return (
         <div className="widget-container">
-            Place for Weather
+            <div>Weather widget</div>
+            {renderWeather(weather)}
+            <div>{date && (date.getHours() + " : " + date.getMinutes() + " : " + date.getSeconds())}</div>
         </div>
     )
 }
