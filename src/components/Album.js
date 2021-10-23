77661cd6383bdf2e692823a3184e13ef374ff9d4
norujumping <norujumping@kano.moe>
2021-10-23 18:54:47 -0400

    merged

--- src/components/Album.js
+++ src/components/Album.js
@@ -2,12 +2,8 @@ import React from 'react'
 
 const Album = () => {
     return (
-<<<<<<< HEAD
-        <div className="widget-container">      
-=======
         <div>
             Album here
->>>>>>> 597e6d08d323223da1f5886995e43f59be287b5c
         </div>
     )
 }
