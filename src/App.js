771c4583a7deba77d621450d5c9d7073e03f919d
norujumping <norujumping@kano.moe>
2021-10-24 00:53:51 -0400

    Time Shift

--- src/App.js
+++ src/App.js
@@ -7,46 +7,60 @@ import { useState, useEffect } from 'react';
 function App() {
   const [weather, weatherSet] = useState('')
   const [date, dateSet] = useState(new Date())
-  const [mainTheme, mainThemeSet] = useState('App')
+  const [dt, dtSet] = useState(0)
   const convertWeather = (weather) => {
-      switch (weather) {
-          case 'Thunderstorm': return 'thunder'
-          case 'Rain': return 'rain'
-          case 'Clear': return 'sunny'
-          default: return 'cloudy'
-      }
+    switch (weather) {
+      case 'Thunderstorm': return 'thunder'
+      case 'Rain': return 'rain'
+      case 'Clear': return 'sunny'
+      default: return 'cloudy'
+    }
   }
   const updateDate = () => {
-    dateSet(new Date());
+    let curDate = new Date()
+    dateSet(curDate);
   }
-  const getTimeOfDay = (hour) => {
-      if (hour <= 5) return 'night'
-      if (hour <= 12) return 'morning'
-      if (hour <= 16) return 'afternoon'
-      if (hour <= 19) return 'evening'
-      else return 'night'
+  const getShiftedDate = (origDate, dt) => {
+    let shiftedDate = new Date(origDate)
+    shiftedDate.setHours(shiftedDate.getHours() + dt)
+    return shiftedDate
+  }
+  const getTimeOfDay = (date) => {
+    let hour = date.getHours()
+    if (hour <= 5) return 'night'
+    if (hour <= 12) return 'morning'
+    if (hour <= 16) return 'afternoon'
+    if (hour <= 19) return 'evening'
+    else return 'night'
   }
 
   useEffect(() => {
-      const updateWeather = () => {
-          let url = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=minutely,hourly,daily&appid=e7d52186e0e22ca020dd281f84eaed33"
-          fetch(url).then(res => res.json()).then((result) => {
-              let curWeather = result.current.weather[0].main
-              weatherSet(convertWeather(curWeather))
-          })
-      }
-      updateWeather()
-      setInterval(updateWeather(), 5000);
-      setInterval(updateDate, 1000);
+    const updateWeather = () => {
+      let url = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=minutely,hourly,daily&appid=e7d52186e0e22ca020dd281f84eaed33"
+      fetch(url).then(res => res.json()).then((result) => {
+        let curWeather = result.current.weather[0].main
+        weatherSet(convertWeather(curWeather))
+      })
+    }
+    updateWeather()
+    setInterval(updateWeather(), 5000);
+    setInterval(updateDate, 1000);
   }, [])  //! Important! [] dependency means called only once
+
+  const trickTime = () => {
+    dtSet(parseInt(prompt("Enter how many hours you want to deviate")))
+  }
+  const getTheme = () => {
+    return getTimeOfDay(getShiftedDate(date, dt)) + '-theme'
+  }
   return (
-    <div className={getTimeOfDay(date)+'-theme'}>
+    <div className={getTheme()}>
       <Animated animationIn="zoomIn" animationOut="fadeOut" isVisible={true}>
-        <header className="App-header">
+        <header className="App-header" onClick={trickTime}>
           Spotomosphere
         </header>
       </Animated>
-      <MainContainer weather={weather} date={date} />
+      <MainContainer weather={weather} date={getShiftedDate(date, dt)} />
 
 
     </div >
