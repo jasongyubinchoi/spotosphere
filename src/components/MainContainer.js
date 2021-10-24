538f351fca122c0745730bdb4baf994821ac6614
Jason Choi <jasonchoi@lawn-143-215-94-139.lawn.gatech.edu>
2021-10-24 01:13:11 -0400

    The all requirement is completed.

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -14,18 +14,7 @@ const MainContainer = ({weather, date}) => {
     }
 
 
-    useEffect(() => {
-        const updateWeather = () => {
-            let url = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=minutely,hourly,daily&appid=e7d52186e0e22ca020dd281f84eaed33"
-            fetch(url).then(res => res.json()).then((result) => {
-                let curWeather = result.current.weather[0].main
-                weatherSet(convertWeather(curWeather))
-            })
-        }
-        updateWeather()
-        setInterval(updateWeather(), 5000);
-        setInterval(updateDate, 1000);
-    }, [])  //! Important! [] dependency means called only once
+    
     return (
         
         <div className="main-container">
